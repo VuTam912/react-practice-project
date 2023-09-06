6e104ea1f1b92e386c684faebf7caebb59c1b457
Vu Tam R <vutam311291@gmail.com>
2023-09-06 11:13:05 +0700

    Boostrap - Header-Responsive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // React Ver 17.0.2
 ReactDOM.render(
